fc2d40667d2d1ef09b3b7072bc218bb1108b11b2
Nikolai Mushegian <nikolai@nexusdev.us>
2017-08-23 19:43:47 -0400

    sketch example cost function

--- src/lpc.sol
+++ src/lpc.sol
@@ -1,6 +1,21 @@
 import 'ds-thing/thing.sol';
 import 'ds-token/token.sol';
 
+contract Example1 is CostFunction {
+    DSValue public feed;
+    uint128 public fee; // % wad fee
+    function Example1(DSValue feed_, uint128 fee_) {
+        feed = feed_;
+        fee = fee_;
+    }
+    function cost(uint128 want_amt) constant returns (uint need_amt) {
+        var price = feed.read();
+        var cost = wmul(price, want_amt);
+        var fee = wdiv(cost, fee);
+        var total = wadd(cost, fee);
+        return total;
+    }
+}
 contract CostFunction {
     // can use `now`
     function cost(uint128 want_amt) constant returns (uint128 need_amt);
@@ -26,7 +41,7 @@ contract LPC is DSThing {
         want.push(msg.sender, c);
     }
     function takeSafe(DSToken have, DSToken want, uint128 wwad, uint128 hwad)
-        // note: take notes
+        // note: `take` notes
     {
         var cost = look(have, want, wwad);
         require(cost <= hwad);
